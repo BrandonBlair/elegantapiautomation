7a411cc985552d64b76dde139b043ec12792ad7b
Brandon Blair <cbrandon.blair@gmail.com>
2021-01-17 13:48:30 -0600

    Initial Commit

--- api.py
+++
@@ -1,9 +0,0 @@
-import time
-
-import requests
-
-start = time.time()
-response = requests.get('http://google.com')
-print(response.text[:200])
-elapsed = time.time() - start
-print(f"API validation took {elapsed} seconds")
