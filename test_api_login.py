f7a310f1a71d61abc58232f041ae76685a195071
Brandon Blair <cbrandon.blair@gmail.com>
2021-06-27 14:41:22 -0500

    Readability

--- test_api_login.py
+++ test_api_login.py
@@ -2,10 +2,13 @@ from requests import Session
 
 
 def test_can_login(logged_in_session):
-    resp = logged_in_session.get(url="http://127.0.0.1:5000/search")
+    resp = logged_in_session.get(
+        url="http://127.0.0.1:5000/search"
+    )
     assert 'Search By ISBN-10' in resp.text
 
-def test_can_skip_login_using_browser(logged_in_session, browser):
+def test_can_skip_login_using_browser(
+    logged_in_session, browser):
     # Load cookies into browser session
     lsession = logged_in_session.cookies.get('LSESSION')
     token = logged_in_session.cookies.get('token')
@@ -15,11 +18,23 @@ def test_can_skip_login_using_browser(logged_in_session, browser):
     browser.get(url="http://127.0.0.1:5000")
 
     # Add the necessary cookies to the browser instance
-    browser.add_cookie({'name': 'LSESSION', 'value': lsession})
-    browser.add_cookie({'name': 'token', 'value': token})
+    browser.add_cookie(
+        {
+            'name': 'LSESSION', 
+            'value': lsession
+        }
+    )
+    browser.add_cookie(
+        {
+            'name': 'token', 
+            'value': token
+        }
+    )
 
     # Attempt to navigate directly to the search page
     browser.get(url="http://127.0.0.1:5000/search")
 
     # Prove that we are logged in successfully
     assert 'Search By ISBN-10' in browser.page_source
+
+    import pdb; pdb.set_trace()
