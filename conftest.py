0543863fa976eb37ecebaf09338792081fa72f94
Brandon Blair <cbrandon.blair@gmail.com>
2021-06-27 14:12:29 -0500

    Log in using backend

---
+++ conftest.py
@@ -0,0 +1,30 @@
+from pytest import fixture
+from requests import Session
+
+from selenium import webdriver
+
+@fixture
+def logged_in_session():
+    session = Session()
+
+    # Perform the first GET to retrieve a cookie (LSESSION)
+    get_resp = session.get(url="http://127.0.0.1:5000/login")
+
+    # Login using a POST
+    login_info = {
+        "email": "brandon@techstep.com",
+        "password": "1234"
+    }
+
+    post_resp = session.post(url="http://127.0.0.1:5000/login", data=login_info)
+
+    if 'Search By Author' not in post_resp.text:
+        raise Exception("Did not log in successfully")
+
+    return session
+
+@fixture
+def browser():
+    chrome_window = webdriver.Chrome()
+    yield chrome_window
+    chrome_window.close()
---
+++ test_api_login.py
@@ -0,0 +1,25 @@
+from requests import Session
+
+
+def test_can_login(logged_in_session):
+    resp = logged_in_session.get(url="http://127.0.0.1:5000/search")
+    assert 'Search By ISBN-10' in resp.text
+
+def test_can_skip_login_using_browser(logged_in_session, browser):
+    # Load cookies into browser session
+    lsession = logged_in_session.cookies.get('LSESSION')
+    token = logged_in_session.cookies.get('token')
+
+    # Navigate to landing page (WARNING: You cannot add cookies until 
+    # the path has been set this way! Don't skip this step!)
+    browser.get(url="http://127.0.0.1:5000")
+
+    # Add the necessary cookies to the browser instance
+    browser.add_cookie({'name': 'LSESSION', 'value': lsession})
+    browser.add_cookie({'name': 'token', 'value': token})
+
+    # Attempt to navigate directly to the search page
+    browser.get(url="http://127.0.0.1:5000/search")
+
+    # Prove that we are logged in successfully
+    assert 'Search By ISBN-10' in browser.page_source
